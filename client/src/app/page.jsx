68c1a47eeff6a1e7a0af5ee084d66ee280a965c0
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-18 18:42:04 +0300

    Added the prettier config file

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,13 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="">
-    
-
-  
-
-      
+      <h1 className="bg-red-500">Hello</h1>
     </main>
-  )
+  );
 }
