d1d1d7c050954a6b6527c23bb282f53587d04482
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-19 10:03:41 +0300

    Created a dynamic routing path

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <h1>Hello nEXT</h1>
       </main>
 
