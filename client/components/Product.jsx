9ac290645e72165495c8a1049eec7796707f91af
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-19 12:43:45 +0300

    Removed the excces files in the parent directory

--- client/components/Product.jsx
+++ client/components/Product.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { ProductStyle } from "../styles/ProductStyle";
 
 const Product = ({product}) => {
     //Extract info from props
     const {Title, Price, Description, Image} = product.attributes;
   return (
-    <div>
+    <ProductStyle>
       <div>
         <img src= {Image.data.attributes.formats.small.url} alt="Awesome product" />
       </div>
       <h2>{Title}</h2>
       <p>{Description}</p>
       <h3>{Price}</h3>
-    </div>
+    </ProductStyle>
   );
 };
 
