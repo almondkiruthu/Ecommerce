3d3a854e45d5cabde4b030fc91e261047b52af4b
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-19 09:54:30 +0300

    Created an about file and tested the css styling modules + page routing

---
+++ client/pages/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/About.module.css'
+
+const About = () => {
+  return (
+    <div className={styles.about}>
+      <h1>About me in Next</h1>
+    </div>
+  )
+}
+
+export default About
