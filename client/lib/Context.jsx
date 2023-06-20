c259fb750955be4fb51ed77a3696149967b5fa95
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-20 18:17:27 +0300

    Created a context to entail all the useState hooks

---
+++ client/lib/Context.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Context, useContext, useState } from 'react'
+
+const context = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default context
