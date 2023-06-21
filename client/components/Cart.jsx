821519c2d5ef3e7694e0238555eea2f71faa3518
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-21 11:28:16 +0300

    Added animations to my components

--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -6,19 +6,35 @@ import {
   Card,
   CardInfo,
   EmptyStyle,
-  Checkout
+  Checkout,
 } from "../styles/CartStyles";
 import { FaShoppingCart } from "react-icons/fa";
 import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
 import { Quantity } from "../styles/ProductsDetail";
 
 const Cart = () => {
-  const { cartItems, setShowCart, onAdd, onRemove, totalPrice } = useStateContext();
+  const { cartItems, setShowCart, onAdd, onRemove, totalPrice } =
+    useStateContext();
   return (
-    <CartWrapper onClick={() => setShowCart(false)}>
-      <CartStyle onClick={(e) => e.stopPropagation()}>
+    <CartWrapper
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      onClick={() => setShowCart(false)}
+    >
+      <CartStyle
+        initial={{ x: "50%" }}
+        animate={{ x: 0 }}
+        exit={{ x: "50%" }}
+        transition={{ type: "tween", stiffness: 100 }}
+        onClick={(e) => e.stopPropagation()}
+      >
         {cartItems.length === 0 && (
-          <EmptyStyle>
+          <EmptyStyle
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 0.4 }}
+          >
             <h1>You have more shopping to do! 😜</h1>
             <FaShoppingCart />
           </EmptyStyle>
@@ -26,7 +42,10 @@ const Cart = () => {
         {cartItems.length >= 1 &&
           cartItems.map((item) => {
             return (
-              <Card>
+              <Card
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={{ opacity: 1, scale: 1, transition: { delay: 0.4 } }}
+              >
                 <img
                   src={item.Image.data.attributes.formats.thumbnail.url}
                   alt={item.title}
