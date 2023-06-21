1dc8524bbc5308e48e4c57b6a042445f4e3934ba
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-06-21 11:59:35 +0300

    Added some animation to my cart span

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -4,7 +4,7 @@ import { FiShoppingBag } from "react-icons/fi";
 import { NavItems, NavStyle } from "../styles/NavStyles";
 import Cart from "./Cart";
 import { useStateContext } from "../lib/context";
-const { AnimatePresence } = require("framer-motion");
+const { AnimatePresence, motion } = require("framer-motion");
 
 const Nav = () => {
   const { showCart, setShowCart, totalQuantities } = useStateContext();
@@ -13,7 +13,9 @@ const Nav = () => {
       <Link href="/">Styled.</Link>
       <NavItems>
         <div onClick={() => setShowCart(true)}>
-          {totalQuantities > 0 && <span>{totalQuantities}</span>}
+          <motion.span animate={{ scale: 1 }} initial={{ scale: 0 }}>
+            {totalQuantities}
+          </motion.span>
           <FiShoppingBag />
           <h3>Cart</h3>
         </div>
